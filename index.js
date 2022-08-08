2ced31200cfa5ec1ac9ac5ad892662f278d9e89e
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-08-08 21:35:43 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 6.2;
+const num2 = 10;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10);
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+const max = (1,2,3,20);
